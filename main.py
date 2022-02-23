b817460c96829f8b24466c95513960aad9f180e2
Fernando Vinicius Goncalves de Souza <fernandovinicius1708@gmail.com>
2022-02-23 03:30:43 -0300

    Estrutura basica do projeto

--- main.py
+++ main.py
@@ -1,13 +1,57 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+import pickle
 
 
-app = Flask('my_app')
+# Instanciando Aplicação Flask c/ Autenticação Simples
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'john'
+app.config['BASIC_AUTH_PASSWORD'] = 'doe'
+basic_auth = BasicAuth(app=app)
 
 
+# Carregando Modelo de RLM Ajustado
+with open(file='./model/lr_model.pkl', mode='rb') as fp:
+    model, scaler, features = pickle.load(file=fp)
+
+
+# Endpoint p/ Cotacao do Valor da Casa
+@app.route('/cotacao', methods=['POST'])
+@basic_auth.required
+def cotacao():
+    # Recebendo dados json
+    data = request.get_json()
+
+    # Parse e Predict do dados recebidos
+    X = [data[col] for col in features]
+    X = scaler.transform([X])
+    price = model.predict(X)[0]
+    price = round(price, 2)
+
+    return jsonify(preco=price)
+
+
+# Endpoint p/ Analise de Sentimento
+@app.route('/sentimento/<frase>')
+@basic_auth.required
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    pol = tb_en.sentiment.polarity
+
+    return jsonify(frase=frase, trad=str(tb_en), polaridade=pol)
+
+
+# Enpoint Teste
 @app.route('/')
 def home():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(
+        host='127.0.0.1',
+        port=5000,
+        debug=True,
+    )
